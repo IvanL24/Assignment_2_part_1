c1652f88bbc6f3da8cc543e04ef91aa1365d52c9
SAITIbrahim <98134542+SAITIbrahim@users.noreply.github.com>
2022-03-06 18:27:20 -0700

    Update StackADT.java
    + Add documentation
    + Additional methods

--- Assignment_2/src/utilities/StackADT.java
+++ Assignment_2/src/utilities/StackADT.java
@@ -2,39 +2,85 @@ package utilities;
 
 import java.io.Serializable;
 
-public interface StackADT<E> extends Serializable{
+/**
+ * <p>
+ * The <code>StackADT</code> interface is designed to be used as a basis for all
+ * stacks. The implementors of this interface will be required to add all the 
+ * functionality.
+ * </p>
+ * 
+ * @param <E> The type of elements this stack holds.
+ */
+public interface StackADT<E> extends Serializable, Iterator<E> {
 
-	/*
-	 * method adds element at the top of the stack
-	 * 
-	 * @param toAdd: to be added at the top of the stack
-	 * @return NullPointerException if empty.
-	 * @return size of stack
-	 */
-	public boolean Push(E toAdd) throws NullPointerException;
+	// ++ Generalized Checks ++
 	
-	/*
-	 * method removes element from top of the stack
+	/**
+	 * The isEmpty method will return either true or false depending upon if the
+	 * stack is empty.
 	 * 
-	 * @return NoSuchElementException if empty.
+	 * @return True if the stack is empty, or false if the stack is occupied.
 	 */
-	public E Pop();
+	boolean isEmpty();
 	
-	/*
-	 * method returns value of the last inserted item on the stack
-	 * 
-	 * @return element value.
-	 * 
+	/**
+	 * The clear method will remove all items from the stack
 	 */
-	public E Peek();
+	void clear();
 	
-	/*
-	 * get the total number of items of the stack
-	 * 
-	 * @return total number of elements
+	/**
+	 * The search method will loop through all items in the stack in search of an item in the stack matching
+	 * the obj parameter.
+	 * @param obj is the desired item to locate in the current stack
+	 * @return If the requested item has been located, then the appropriate index will be returned.
 	 */
-	public int size();
-
-	public void clear();
+	int search(E obj);
+	
+	/**
+	 * The equals method will check to see if two separate stacks match each-other. 
+	 * @param that is the stack used to compare against.
+	 * @return True if both stacks are equal to each other, otherwise, false if they don't match.
+	 */
+	boolean equals(StackADT<E> that);
+	
+	/**
+	 * The size method will report the current element count in this stack
+	 * 
+	 * @return The total numbers of elements in the stack
+	 */
+	int size();
+	
+	/**
+	 * The toArray method will convert the current stack into an array of Objects
+	 * @return An array of objects which correspond to what is found in this stack
+	 */
+	Object[] toArray();
+	
+	/**
+	 * The toArray method will convert the current stack into an array of E type\
+	 * @param copy The desired array to copy
+	 * @return An array of objects which correspond to what is passed as parameter copy
+	 */
+	E[] toArray(E[] copy);
+	
+	// ++ Core Functionality ++
+	
+	/**
+	 * The push method will add an item to the top of this stack.
+	 * @param item the item to add to the top of the stack
+	 */
+	void push(E item);
+	
+	/**
+	 * The pop method will remove and return the item at the top of the stack
+	 * @return item of type E (top of the stack)
+	 */
+	E pop();
+	
+	/**
+	 * The peek method will return the item at the top of the stack (without removing it)
+	 * @return item of type E (top of the stack)
+	 */
+	E peek();
 	
 }
