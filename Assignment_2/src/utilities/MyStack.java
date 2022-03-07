04175e44a3c3b9be577f6ab776f6134846edf568
Luckymarc <93414005+Luckymeanima@users.noreply.github.com>
2022-03-06 20:01:21 -0700

    MyStack partly done

--- Assignment_2/src/utilities/MyStack.java
+++ Assignment_2/src/utilities/MyStack.java
@@ -1,33 +1,110 @@
 package utilities;
 
+import java.util.NoSuchElementException;
+
 public class MyStack<E> implements StackADT<E>  {
+	
+	
+	  private Object[] array;
+	  private E top;
+	  private int capacity;
+	  private int size;
 
 	public MyStack(){
-		// TODO Auto-generated constructor stub
+		
+	    array = new Object[capacity];
+	    top = null;
+	    size = 0;
 	}
 
 	@Override
-	public boolean Push(E toAdd) throws NullPointerException {
+	public boolean hasNext() {
+		
+		return false;
+	}
+
+	@Override
+	public E next() throws NoSuchElementException {
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		
+		return this.size == 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int search(E obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(StackADT<E> that) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public E Pop() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public E Peek() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return this.size;
 	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E[] toArray(E[] copy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(E item) {
+		if(isFull()) {
+			System.out.println("Stack is Full");
+		}
+		
+		this.size++;
+		
+	}
+
+	private boolean isFull() {
+	
+		return size() == capacity;
+	}
+
+	@Override
+	public E pop() {
+		
+		if(isEmpty()) {
+			System.out.println("Stack is Empty");
+		
+		}
+		
+		this.size--;
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		
+		return top;
+	}
+
+	
 
 }
